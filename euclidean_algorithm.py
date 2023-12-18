e7dc4b89596b2ec2095cdd89cf909ca4af0c1622
Jean-Pierre Chauvel <jean.p.chauvel@gmail.com>
2023-12-18 16:27:15 -0500

    Adding euclidean algoritm

---
+++ euclidean_algorithm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def gcd(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def main():
+    print(gcd(270, 192))
+
+
+if __name__ == '__main__':
+    main()
